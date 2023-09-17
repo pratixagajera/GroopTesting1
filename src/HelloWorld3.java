a1dc803b367ecd3b9ee22af2957056eb1e40f7d7
Pratixa Donga <pratixagajera2609@gmail.com>
2023-09-17 13:55:26 +0100

    fourth commit

---
+++ src/HelloWorld3.java
@@ -0,0 +1,9 @@
+public class HelloWorld3 {
+    public static void main(String[] args) {
+        submain("kjdhdwwvf", "udhewuigfewi","uhy8etf");
+    }
+    public static void submain(String one, String two, String three){
+        System.out.println(two+one);
+        System.out.println(three+one);
+    }
+}
